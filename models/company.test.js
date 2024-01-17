2e7104f3fdbaae9924ed13202f1e9f45259bfbed
eduardopedroza <ea.pedroza23@gmail.com>
2024-01-17 14:04:38 +0100

    added job data to GET /companies/:handle

--- models/company.test.js
+++ models/company.test.js
@@ -163,6 +163,15 @@ describe("get", function () {
       description: "Desc1",
       numEmployees: 1,
       logoUrl: "http://c1.img",
+      jobs: [
+        {
+          id: expect.any(Number), 
+          title: 'j1',
+          salary: 100000,
+          equity: '0',
+          companyHandle: 'c1'
+        }
+      ]
     });
   });
 
